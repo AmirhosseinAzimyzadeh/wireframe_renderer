9d83adac278570d7dd0585042f23e21227754ed3
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-04 09:21:15 +0330

    initial implementation

---
+++ src/models/plane/Plane.ts
@@ -0,0 +1,23 @@
+import Geometry from "../../utils/Geometry.ts";
+import Point from "../point/Point.ts";
+import Vector from "../vector/Vector.ts";
+
+class Plane {
+  public normal: Vector;
+  public point: Point;
+
+  constructor(p: Point, n: Vector) {
+    this.normal = n;
+    this.point = p;
+  }
+
+  isInPlane(p: Point): boolean {
+    if (p.isEqual(this.point)) return true;
+    else {
+      const tempVec = Geometry.subtractPoints(p, this.point);
+      return Geometry.innerProduct(tempVec, this.normal) === 0;
+    }
+  }
+}
+
+export default Plane;
