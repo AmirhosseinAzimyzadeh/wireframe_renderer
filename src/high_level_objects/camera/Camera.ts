95d00c068daa4faa880fa1a8bfeac85c7357445a
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-04 09:20:50 +0330

    initial implementation

---
+++ src/high_level_objects/camera/Camera.ts
@@ -0,0 +1,13 @@
+class Camera {
+
+  private _height: number;
+  private _width: number;
+  
+
+  constructor(w: number, h: number) {
+    this._height = h;
+    this._width = w;
+  }
+}
+
+export default Camera;
