0259dcbe27c485b0d8b79cb4b96148d22daedaff
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-07 16:53:44 +0330

    initial implementation (with no test !)

---
+++ src/utils/renderer/projectTriangle.ts
@@ -0,0 +1,28 @@
+import Camera from "../../high_level_objects/camera/Camera.ts";
+import Triangle from "../../models/triangle/Triangle.ts";
+import Ray from "../../models/ray/Ray.ts";
+import Geometry from "../geometry/Geometry.ts";
+
+/** returns projected triangle on a plane */
+function projectTriangle(
+  camera: Camera,
+  triangle: Triangle
+): Triangle {
+  const projectedPoints = triangle.points.map((currentPoint) => {
+    const ray: Ray = Ray.createFromPoints(
+      currentPoint,
+      camera.viewPoint,
+    );
+    const intersection = Geometry.rayPlaneIntersection(camera.plane, ray);
+    if (intersection instanceof Ray) return currentPoint;
+    return intersection;
+  });
+
+  return new Triangle(
+    projectedPoints[0],
+    projectedPoints[1],
+    projectedPoints[2],
+  );
+}
+
+export default projectTriangle;
