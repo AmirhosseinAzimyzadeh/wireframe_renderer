8ed61ae55be33a355e06c7224174426d75eb8efe
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-11 22:37:45 +0330

    update test

--- src/utils/renderer/renderer.test.ts
+++ src/utils/renderer/renderer.test.ts
@@ -6,6 +6,7 @@ import Plane from "../../models/plane/Plane.ts";
 import Vector from "../../models/vector/Vector.ts";
 import render from "./render.ts";
 import renderOnCanvas from "../canvas_adaptor/canvasAdaptor.ts";
+import Geometry from "../geometry/Geometry.ts";
 
 
 Deno.test("Renderer", () => {
@@ -32,14 +33,14 @@ Deno.test("Renderer", () => {
 });
 
 Deno.test("Renderer 2", async () => {
-  const p1 = new Point(0, 0, 0);
-  const p2 = new Point(0, 0, 500);
-  const p3 = new Point(-500, 0, 0);
-  const p4 = new Point(-500, 0, 500);
-  const p5 = new Point(-500, 500, 500);
-  const p6 = new Point(0, 500, 500);
-  const p7 = new Point(0, 500, 0);
-  const p8 = new Point(-500, 500, 0);
+  const p1 = new Point(250, 250, 250);
+  const p2 = new Point(250, 250, 750);
+  const p3 = new Point(-250, 250, 250);
+  const p4 = new Point(-250, 250, 750);
+  const p5 = new Point(-250, 750, 750);
+  const p6 = new Point(250, 750, 750);
+  const p7 = new Point(250, 750, 250);
+  const p8 = new Point(-250, 750, 250);
 
   const t1 = new Triangle(p1, p2, p3);
   const t2 = new Triangle(p2, p3, p4);
@@ -47,8 +48,6 @@ Deno.test("Renderer 2", async () => {
   const t4 = new Triangle(p1, p6, p7);
   const t5 = new Triangle(p6, p7, p8);
   const t6 = new Triangle(p5, p6, p8);
-
-  
   const t7 = new Triangle(p5, p3, p8);
   const t8 = new Triangle(p3, p4, p5);
   const t9 = new Triangle(p1, p2, p6);
@@ -56,17 +55,34 @@ Deno.test("Renderer 2", async () => {
   const t11 = new Triangle(p6, p7, p8);
   const t12 = new Triangle(p3, p1, p8);
 
+  const triangles = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12];
 
-  const plane = new Plane(new Point(0, 100, -100), new Vector(0, -10, -10));
-  const camera = new Camera(6000, 3000, plane, 100000);
 
-  const data = render(camera, [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12]);
+
+  const plane = new Plane(new Point(100, 0, 0), new Vector(-10, 0, 0));
+  const camera = new Camera(3000, 3000, plane, 100000);
+
   
-  data.paths.forEach((p) => {
-    p.forEach((t) => {
-      console.log(`(${t.x}, ${t.y})`)
-    })
-  });
+  // data.paths.forEach((p) => {
+  //   p.forEach((t) => {
+  //     console.log(`(${t.x}, ${t.y})`)
+  //   })
+  // });
 
-  await renderOnCanvas(data);
+  for (let i=0.01 ; i< 0.5 ;i+=0.01 ) {
+    // rotate
+    const data = render(camera, triangles);
+    await renderOnCanvas(data, `${i}_image.png`);
+    triangles.forEach((t) => {
+    t.points.forEach((p) => {
+      const rotatedPoint = Geometry.rotateZPoint(p, i);
+      // rotatedPoint = Geometry.rotateYPoint(p, Math.PI / 3);
+      p.x = rotatedPoint.x;
+      p.y = rotatedPoint.y;
+      p.z = rotatedPoint.z;
+    }); 
+  })
+  }
+
+
 });
